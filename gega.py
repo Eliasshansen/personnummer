bb0ae57fbae6381b8471901a20fcc21cf2245e0a
Elias Sundström Hansen <elsu0317@storsjogymnasiet.se>
2023-03-21 09:23:19 +0100

    personnummer

---
+++ gega.py
@@ -0,0 +1,15 @@
+import random
+
+looping=True
+
+while looping:
+    url = ""
+
+    req =requests.get(url)
+
+    json = req.json()
+    print(req)
+    lista = jsondata ["results"]
+    print(lista)
+
+    entill0 

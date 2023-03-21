5676a3a209466eca0aaea681036555b23fb1b363
Elias Sundström Hansen <elsu0317@storsjogymnasiet.se>
2023-03-21 10:13:53 +0100

    personnummer

--- gega.py
+++ gega.py
@@ -1,15 +1,18 @@
 import random
+import requests
 
 looping=True
-
 while looping:
-    url = ""
+    randomtal = random.randint(1, 1000)
+    url = f"https://skatteverket.entryscape.net/rowstore/dataset/b4de7df7-63c0-4e7e-bb59-1f156a591763/json?_offset={random}&_limit=1"
 
     req =requests.get(url)
 
     json = req.json()
     print(req)
-    lista = jsondata ["results"]
+    lista = json["results"]
     print(lista)
 
-    entill0 
+    svar=input("vill du köra programmet en gång till? j/n")
+    if (svar == "n" or svar == "N"):
+        break
